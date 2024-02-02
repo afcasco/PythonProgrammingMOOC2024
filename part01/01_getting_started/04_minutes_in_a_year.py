47b11a911eef2bfbf006de0de0192060c7544947
afcasco <7701272+afcasco@users.noreply.github.com>
2024-02-02 19:08:42 +0100

    add 05_print_code

---
+++ part01/01_getting_started/04_minutes_in_a_year.py
@@ -0,0 +1,5 @@
+# Please write a program which prints out the number of minutes in a year. Use Python code to perform the calculation,
+# as in the previous code example.
+
+print("Minutes in a year:")
+print(365 * 24 * 60)
---
+++ part01/01_getting_started/05_print_code.py
@@ -0,0 +1,5 @@
+# Please write a program which prints out the following:
+#
+# print("Hello there!")
+
+print('print("Hello there!")')
